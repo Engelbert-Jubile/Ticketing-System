0380063e88f7cbd9a8362939f3f874d679aec7b2
Engelbert Jubile <jubile82@gmail.com>
2026-03-11 13:12:11 +0700

    fix app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -68,3 +68,9 @@ return Application::configure(basePath: dirname(__DIR__))
         // Optional: custom exception handler
     })
     ->create();
+
+// Permanent fix for Linux case-sensitive filesystems:
+// this project stores application classes in "App/" (capital A), not "app/".
+$app->useAppPath(__DIR__.'/../App');
+
+return $app;
