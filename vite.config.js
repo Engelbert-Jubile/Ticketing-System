ddf87032a6c4fe30b82d4ecdfd1e6fb8df72c059
Engelbert Jubile <jubile82@gmail.com>
2026-09-14 20:56:02 +0700

    Changes to be committed: modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
         'resources/css/app.css',
         'resources/css/animations.css',
         'resources/js/app.js',
-        'resources/js/auth/register-extra.js',
         'resources/js/dashboard.js',
         'resources/js/legacy-entry.js',
       ],
